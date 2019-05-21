bdb3b2a60bfc892c3758dfd500d3692e754fb18a
Will Crichton <wcrichto@cs.stanford.edu>
2019-05-21 10:23:17 -0700

    Center canvas, move robot

--- server.py
+++ server.py
@@ -14,10 +14,17 @@ target_height = 150
 
 def recv(client, server, message):
     data = json.loads(message)
-    x, y = data['x'], data['y']
+    x, y, ty = data['x'], data['y'], data['type']
     x = x * target_width / client_width
-    y = y * target_height / client_height
-    p.move(target_width - x, y)
+    x = target_width - x - target_width / 2
+    y = y * target_height / client_height - target_height / 2
+    if ty == 'start':
+        p.sprint(x, y)
+        p.down()
+    elif ty == 'move':
+        p.move(x, y, feed=10000)
+    elif ty == 'end':
+        p.up()
         
 
 server.set_fn_message_received(recv)
